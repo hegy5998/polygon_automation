1c422ce5fc52d8408e1770b7e5d2983a638d7ab5
hegy5998 <hegy5998@gmail.com>
2025-06-16 16:50:54 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,10 @@ import folium
 from streamlit_folium import st_folium
 import streamlit.components.v1 as components
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0'}
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
+    'Referer': 'polygonautomation-cm9hhicmdvkxc6maiyqpsp.streamlit.app'
+}
 
 @st.cache_data(show_spinner=False)
 def fetch_osm_geojson(query):
